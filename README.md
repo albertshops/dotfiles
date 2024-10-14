ca5b452c9d50d36e6e0d19b281e0159a852af546
Ali <alasdairwstores@gmail.com>
2024-10-14 14:09:46 +0100

    links.sh

--- README.md
+++
@@ -1,5 +0,0 @@
-ln -s ~/dotfiles/nvim ~/.config/nvim
-ln -s ~/dotfiles/tmux ~/.config/tmux
-ln -s ~/dotfiles/kitty ~/.config/kitty
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/aerospace ~/.aerospace
---
+++ links.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ln -s ~/dotfiles/nvim ~/.config/nvim
+ln -s ~/dotfiles/tmux ~/.config/tmux
+ln -s ~/dotfiles/kitty ~/.config/kitty
+ln -s ~/dotfiles/zshrc ~/.zshrc
+ln -s ~/dotfiles/aerospace ~/.aerospace
