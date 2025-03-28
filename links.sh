a8959d9a17873cbe5820ea5ee5408b35fd615890
Ali <alasdairwstores@gmail.com>
2025-03-28 09:53:32 +0000

    add ghostty to links

--- links.sh
+++ links.sh
@@ -19,3 +19,4 @@ create_symlink ~/dotfiles/kitty ~/.config/kitty
 create_symlink ~/dotfiles/zshrc ~/.zshrc
 create_symlink ~/dotfiles/aerospace.toml ~/.aerospace.toml
 create_symlink ~/dotfiles/lazygit.yml ~/Library/Application\ Support/lazygit/config.yml
+create_symlink ~/dotfiles/ghostty ~/Library/Application\ Support/com.mitchellh.ghostty/config
