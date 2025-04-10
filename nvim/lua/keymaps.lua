aef7cd68646d7afd7e9bd1768ced5fb328ea50fd
Ali <alasdairwstores@gmail.com>
2025-04-10 11:12:56 +0100

    surround visual selection with html tag

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -19,6 +19,14 @@ map("n", "K", ":lua vim.lsp.buf.hover()<CR>", { desc = "Hover" })
 map("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>", { desc = "Open diagnostics float" })
 map("n", "gd", ":lua vim.lsp.buf.definition()<CR>", { desc = "Go to definition" })
 map("n", "<leader>a", ":lua vim.lsp.buf.code_action()<CR>", { desc = "Code action" })
+map("n", "<leader>r", function()
+	local original_input = vim.ui.input
+	vim.ui.input = function(opts, on_confirm)
+		opts.default = "" -- Clear the default input
+		original_input(opts, on_confirm)
+	end
+	vim.lsp.buf.rename()
+end, { desc = "Rename symbol" })
 
 -- toggle comments
 map("n", "<leader>c", ":lua require('Comment.api').toggle.linewise.current()<CR>", { desc = "Toggle comment" })
@@ -59,6 +67,7 @@ map("n", "<leader>t", function()
 	vim.cmd("startinsert")
 end, { desc = "Open terminal" })
 
+-- toggle supermaven
 map("i", "<C-t>", function()
 	local api = require("supermaven-nvim.api")
 	api.toggle()
@@ -75,3 +84,40 @@ map("n", "<C-t>", function()
 	api.toggle()
 	print("Supermaven: " .. tostring(api.is_running()))
 end, { desc = "Supermaven Toggle" })
+
+-- surround
+vim.keymap.set("v", "<leader>s", function()
+	local tag = vim.fn.input("Enter tag: ")
+	if tag == "" then
+		return
+	end
+
+	local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+	vim.api.nvim_feedkeys(esc, "x", false)
+
+	local start_pos = vim.fn.getpos("'<")
+	local end_pos = vim.fn.getpos("'>")
+	local start_line = start_pos[2] - 1
+	local start_col = start_pos[3] - 1
+	local end_line = end_pos[2] - 1
+	local end_col = end_pos[3]
+
+	local lines = vim.api.nvim_buf_get_lines(0, start_line, end_line + 1, false)
+
+	-- Clamp end_col to line length
+	local end_line_text = lines[#lines]
+	end_col = math.min(end_col, #end_line_text)
+
+	lines[#lines] = end_line_text:sub(1, end_col)
+	lines[1] = lines[1]:sub(start_col + 1)
+
+	local selected_text = table.concat(lines, "\n"):gsub("^%s+", ""):gsub("%s+$", "")
+	local wrapped = "<" .. tag .. ">" .. selected_text .. "</" .. tag .. ">"
+
+	vim.api.nvim_buf_set_text(0, start_line, start_col, end_line, end_col, vim.split(wrapped, "\n"))
+
+	-- Move cursor to just after opening tag
+	local col_after_tag = start_col + #("<" .. tag)
+	vim.api.nvim_win_set_cursor(0, { start_line + 1, col_after_tag })
+	vim.cmd("startinsert")
+end, { desc = "Wrap trimmed selection with XML tag and format" })
