0339634258ae3fcc23d9fe64313b46d8e09fb668
Ali <alasdairwstores@gmail.com>
2025-05-07 13:34:54 +0100

    vimade

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,4 +1,14 @@
 return {
 	"stevearc/oil.nvim",
-	opts = {},
+	opts = {
+		use_default_keymaps = false,
+		keymaps = {
+			["<CR>"] = "actions.select",
+			["-"] = { "actions.parent", mode = "n" },
+		},
+		view_options = {
+			show_hidden = true,
+		},
+		columns = {},
+	},
 }
